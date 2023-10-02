46e874bfbec5437fac3ddad2d803ae1cdefd1ffc
moisedegboe14 <moisedegboe095@gmail.com>
2023-10-02 13:07:25 +0200

    Nouveau commit

---
+++ Test_FizzBuzz.py
@@ -0,0 +1,10 @@
+import unittest
+from FizzBuzz import *
+
+class Test_FizzBuzz (unittest.TestCase):
+    def setUp(self):
+        self.instance = FizzBuzz()
+
+
+    def test_affiche_sans_parametre(self):
+        self.assertEqual(self.instance.affiche(),"12")
