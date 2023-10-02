0803045996db65c6f0024232dbefc2ad09b65a7a
moisedegboe14 <moisedegboe095@gmail.com>
2023-10-02 13:52:58 +0200

    new

--- Test_FizzBuzz.py
+++ Test_FizzBuzz.py
@@ -7,7 +7,7 @@ class Test_FizzBuzz (unittest.TestCase):
 
 
     def test_affiche_sans_parametre(self):
-        self.assertEqual(self.instance.affiche(),"12")
+        self.assertEqual(self.instance.affiche(),"12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829FrisBee3132Fizz34BuzzFizz3738FizzBuzz41Fizz4344FrisBee4647Fizz49BuzzFizz5253FizzBuzz56Fizz5859FrisBee6162Fizz64BuzzFizz6768FizzBuzz71Fizz7374FrisBee7677Fizz79BuzzFizz8283FizzBuzz86Fizz8889FrisBee9192Fizz94BuzzFizz9798FizzBuzz")
 
 if __name__ == '__main__':
     unittest.main()
