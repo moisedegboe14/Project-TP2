121e762ee5497f515b5397fddf500918e47c8cde
moisedegboe14 <moisedegboe095@gmail.com>
2023-10-02 12:50:36 +0200

    Nouveau

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,14 +3,14 @@ from fizzbuzz import affiche
 
 class TestFizzBuzz(unittest.TestCase):
 
-    def test_affiche_without_parameters(self):
+    def test_affiche_sans_parameter(self):
         result = affiche()
         self.assertEqual(result, "")
 
-    def test_affiche_with_parameter(self):
+    def test_affiche_avec_parameter(self):
         self.assertEqual(affiche(15), "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee")
 
-    def test_affiche_with_range(self):
+    def test_affiche_avec_range(self):
         self.assertEqual(affiche(5, 10), "BuzzFizz78FizzBuzz")
 
 if __name__ == '__main__':
